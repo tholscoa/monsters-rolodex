000f091676e2385027dc89cc8e2c9e9307c48b8b
takinnuoye <tolulope.akinnuoye@ab-mfbnigeria.com>
2020-08-17 15:48:54 +0100

    creating a class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            String : "Initial text",
+        }
+    }
+
+    render(){
+        return(
+            <div className='App'>
+                <img src={logo} />
+                { this.state.String } <br/>
+                <button onClick={()=>this.setState({String:"Yes"})}>Change Text</button>
+            </div>
+        )
+    }
 }
+
+// function App() {
+//     return ( <
+//         div className = "App" >
+//         <header className = "App-header" >
+//         <img src = { logo }
+//         className = "App-logo"
+//         alt = "logo" / >
+//         <p>
+//         Hello </p> 
+//         <a className = "App-link" href = "https://reactjs.org" target = "_blank" rel = "noopener noreferrer">
+//         Learn React </a> </header> 
+//         </div>
+//     );
+// }
 
 export default App;
